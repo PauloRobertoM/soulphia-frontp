800466a529c4fe5255f5fb0fb4c63d552fd832d2
Paulo Alves <front@agencianovi.com.br>
2019-08-08 18:48:37 -0300

    av

--- index.php
+++ index.php
@@ -13,4 +13,50 @@
 		</div><!-- comece-agora -->
 	</section><!-- vitrine -->
 
+	<section class="como-funciona">
+		<div class="container">
+			<img src="assets/images/borboleta-cinza.png" alt="Borboleta de origami" id="borboleta1" />
+			<img src="assets/images/borboleta-cinza.png" alt="Borboleta de origami" id="borboleta2" />
+			<h1>COMO <span>FUNCIONA</span></h1>
+			<div class="row">
+				<div class="col-md-3 col-sm-3">
+					<div class="item item1">
+						<div class="img">
+							<img src="assets/images/icone-video-aulas.png" alt=""/>
+						</div><!-- img -->
+						<h4>Video-aulas</h4>
+						<p>Desenvolva sua capacidade de entender o inglês falado com Americanos.</p>
+					</div><!-- item -->
+				</div><!-- md-3 -->
+				<div class="col-md-3 col-sm-3">
+					<div class="item">
+						<div class="img">
+							<img src="assets/images/icone-master-class.png" alt=""/>
+						</div><!-- img -->
+						<h4>MasterClass</h4>
+						<p>Aprenda assistindo autlas ao vivo com os melhores professores do Soulphia.</p>
+					</div><!-- item -->
+				</div><!-- md-3 -->
+				<div class="col-md-3 col-sm-3">
+					<div class="item">
+						<div class="img">
+							<img src="assets/images/icone-aulas-em-grupo.png" alt=""/>
+						</div><!-- img -->
+						<h4>Aulas em Grupo</h4>
+						<p>Pratique sua conversação com grupos de até seis pessoas.</p>
+					</div><!-- item -->
+				</div><!-- md-3 -->
+				<div class="col-md-3 col-sm-3">
+					<div class="item">
+						<div class="img">
+							<img src="assets/images/icone-aulas-individuais.png" alt=""/>
+						</div><!-- img -->
+						<h4>Aulas Individuais</h4>							
+						<p>Acelere sua fluência escolhendo o tutor e o tema que mais te agrade.</p>
+					</div><!-- item -->
+				</div><!-- md-3 -->
+			</div><!-- row -->
+		</div><!-- container -->
+	</section><!-- como-funciona -->
+
 <?php include 'components/footer.php'; ?>
