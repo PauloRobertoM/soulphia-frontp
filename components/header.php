6b8c672c903b06e5532c8496559ca94d36b72eaf
Paulo Alves <front@agencianovi.com.br>
2019-08-07 23:28:28 -0300

    a

---
+++ components/header.php
@@ -0,0 +1,35 @@
+<!DOCTYPE html>
+<html lang="pt-BR">
+    <head>
+        <meta charset="UTF-8">
+        <meta http-equiv="Content-Type" content="text/html; charset=utf-8">
+        <meta http-equiv="content-language" content="pt-br">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no">
+        <meta name="author" content="Soulphia">
+        <meta name="keywords" content="ingles, nativo, soulphia, professora nativa, nova york, new york, america, americano, lingua estrangeira, aula, aula de ingles, mudar de vida, ensino de ingles, ensino a distancia, ead, estados unidos, usa">
+        <meta name="description" content="Ensinar inglês não só as recompensam financeiramente, mas também proporciona uma nova maneira de enxergar o mundo de forma mais positiva, acreditando que é possível mudar de vida e seguir em frente, além de terem a auto-estima elevada.">
+        <meta name="reply-to" content="soulphia@soulphia.com">
+        <meta name="theme-color" content="#EE1B48">
+        <title>Soulphia</title>
+
+        <link rel="icon" href="assets/images/favicon.png" type="image/x-icon" />
+
+        <link rel='stylesheet' href='assets/css/bootstrap.min.css' type='text/css' />
+        <link rel='stylesheet' href='assets/css/hover-min.css' type='text/css' />
+        <link rel='stylesheet' href='assets/css/owl.carousel.min.css' type='text/css' />
+        <link rel='stylesheet' href='assets/css/style.css' type='text/css' />
+    </head>
+    <body>
+        <header>
+            <div class="nav menu">
+                <nav class="navbar navbar-default" id="meuMenu">
+                    <div class="container">
+                        <div class="navbar-header">
+                            <a class="navbar-brand" href="index.php"><img class="logo" src="assets/images/logo-soulphia.png" alt="Logo" /></a>
+                            <a class="btn-aluno">Já sou aluno</a>
+                        </div><!-- .navbar-header -->
+                    </div><!-- container -->
+                </nav><!-- .navbar -->
+            </div><!-- .nav -->
+        </header>
---
+++ index.php
@@ -0,0 +1,16 @@
+<?php include 'components/header.php'; ?>
+
+	<section class="vitrine">
+		<img src="assets/images/vitrine.jpg" />
+		<div class="comece-agora">
+			<h1>Se você falasse inglês sua vida seria totalmente diferente.</h1>
+			<form>
+				<input type="text" class="form-control" placeholder="Nome" />
+				<input type="email" class="form-control" placeholder="E-mail" />
+				<input type="tel" class="form-control" placeholder="Telefone" />
+				<button>COMECE AGORA</button>
+			</form>
+		</div><!-- comece-agora -->
+	</section><!-- vitrine -->
+
+<?php include 'components/footer.php'; ?>
