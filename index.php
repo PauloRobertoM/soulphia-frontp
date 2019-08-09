e8ef6b6908ffd52033ab1614ab46c082bc900c22
Paulo Alves <front@agencianovi.com.br>
2019-08-09 00:22:57 -0300

    avançando

--- index.php
+++ index.php
@@ -17,7 +17,7 @@
 		<div class="container">
 			<img src="assets/images/borboleta-cinza.png" alt="Borboleta de origami" id="borboleta1" />
 			<img src="assets/images/borboleta-cinza.png" alt="Borboleta de origami" id="borboleta2" />
-			<h1>COMO <span>FUNCIONA</span></h1>
+			<h1>Nossos <span>Produtos</span></h1>
 			<div class="row">
 				<div class="col-md-3 col-sm-3">
 					<div class="item item1">
@@ -25,16 +25,7 @@
 							<img src="assets/images/icone-video-aulas.png" alt=""/>
 						</div><!-- img -->
 						<h4>Video-aulas</h4>
-						<p>Desenvolva sua capacidade de entender o inglês falado com Americanos.</p>
-					</div><!-- item -->
-				</div><!-- md-3 -->
-				<div class="col-md-3 col-sm-3">
-					<div class="item">
-						<div class="img">
-							<img src="assets/images/icone-master-class.png" alt=""/>
-						</div><!-- img -->
-						<h4>MasterClass</h4>
-						<p>Aprenda assistindo autlas ao vivo com os melhores professores do Soulphia.</p>
+						<p>Desenvolva sua capacidade de entender o inglês falando com Americanos.</p>
 					</div><!-- item -->
 				</div><!-- md-3 -->
 				<div class="col-md-3 col-sm-3">
@@ -44,6 +35,17 @@
 						</div><!-- img -->
 						<h4>Aulas em Grupo</h4>
 						<p>Pratique sua conversação com grupos de até seis pessoas.</p>
+					</div><!-- item -->
+				</div><!-- md-3 -->
+				<img src="assets/images/borboleta-cinza.png" alt="Borboleta de origami" id="borboleta3" />
+				<img src="assets/images/borboleta-cinza.png" alt="Borboleta de origami" id="borboleta4" />
+				<div class="col-md-3 col-sm-3">
+					<div class="item">
+						<div class="img">
+							<img src="assets/images/icone-master-class.png" alt=""/>
+						</div><!-- img -->
+						<h4>MasterClass</h4>
+						<p>Aprenda assistindo autlas ao vivo com os melhores professores do Soulphia.</p>
 					</div><!-- item -->
 				</div><!-- md-3 -->
 				<div class="col-md-3 col-sm-3">
@@ -58,5 +60,73 @@
 			</div><!-- row -->
 		</div><!-- container -->
 	</section><!-- como-funciona -->
+
+	<section class="jornada">
+		<img src="assets/images/bg-jornada.svg" alt="" class="bg-jornada" />
+		<img src="assets/images/boneco-bandeira.svg" alt="" class="boneco-bandeira" />
+		<div class="conteudo">
+			<h1>O Soulphia é para<br /><span>TODOS os alunos!</span></h1>
+			<h4>Do básico ao avançado</h4>
+			<a href="">Comece sua jornada</a>
+		</div><!-- conteudo -->
+	</section><!-- jornada -->
+
+	<section class="porque">
+		<div class="container">
+			<h1>Por que <span>Soulphia?</span></h1>
+			<div class="panel-group" id="accordion" role="tablist" aria-multiselectable="true">
+                <div class="panel panel-default">
+                	<div class="panel-heading" role="tab" id="heading0">
+                        <h4 class="panel-title">
+                            <a role="button" data-toggle="collapse" class="accordion-toggle collapsed" data-parent="#accordion" href="#collapse0" aria-expanded="false" aria-controls="collapse0">Tutoras Americanas</a>
+                        </h4>
+                    </div><!-- panel-heading -->
+                    <div id="collapse0" class="panel-collapse collapse" role="tabpanel" aria-labelledby="heading0">
+                        <div class="panel-body">
+                            <p>Tudo começou com muita observação de Tiago e Felipe, os fundadores do Soulphia.</p>
+                        </div><!-- panel-body -->
+                    </div><!-- collapseOne -->
+
+                    <div class="panel-heading" role="tab" id="heading1">
+                        <h4 class="panel-title">
+                            <a role="button" data-toggle="collapse" class="accordion-toggle collapsed" data-parent="#accordion" href="#collapse1" aria-expanded="false" aria-controls="collapse1">Tutoras Americanas</a>
+                        </h4>
+                    </div><!-- panel-heading -->
+                    <div id="collapse1" class="panel-collapse collapse" role="tabpanel" aria-labelledby="heading1">
+                        <div class="panel-body">
+                            <p>Tudo começou com muita observação de Tiago e Felipe, os fundadores do Soulphia.</p>
+                        </div><!-- panel-body -->
+                    </div><!-- collapseOne -->
+
+                    <div class="panel-heading" role="tab" id="heading2">
+                        <h4 class="panel-title">
+                            <a role="button" data-toggle="collapse" class="accordion-toggle collapsed" data-parent="#accordion" href="#collapse2" aria-expanded="false" aria-controls="collapse2">Estude a  seu tempo</a>
+                        </h4>
+                    </div><!-- panel-heading -->
+                    <div id="collapse2" class="panel-collapse collapse" role="tabpanel" aria-labelledby="heading2">
+                        <div class="panel-body">
+                            <p>Tudo começou com muita observação de Tiago e Felipe, os fundadores do Soulphia.</p>
+                            <p>Tudo começou com muita observação de Tiago e Felipe, os fundadores do Soulphia.</p>
+                            <p>Tudo começou com muita observação de Tiago e Felipe, os fundadores do Soulphia.</p>
+                            <p>Tudo começou com muita observação de Tiago e Felipe, os fundadores do Soulphia.</p>
+                            <p>Tudo começou com muita observação de Tiago e Felipe, os fundadores do Soulphia.</p>
+                            <p>Tudo começou com muita observação de Tiago e Felipe, os fundadores do Soulphia.</p>
+                        </div><!-- panel-body -->
+                    </div><!-- collapseOne -->
+
+                    <div class="panel-heading" role="tab" id="heading3">
+                        <h4 class="panel-title">
+                            <a role="button" data-toggle="collapse" class="accordion-toggle collapsed" data-parent="#accordion" href="#collapse3" aria-expanded="false" aria-controls="collapse3">Estude a  seu tempo</a>
+                        </h4>
+                    </div><!-- panel-heading -->
+                    <div id="collapse3" class="panel-collapse collapse" role="tabpanel" aria-labelledby="heading3">
+                        <div class="panel-body">
+                            <p>Tudo começou com muita observação de Tiago e Felipe, os fundadores do Soulphia.</p>
+                        </div><!-- panel-body -->
+                    </div><!-- collapseOne -->
+                </div><!-- panel -->
+            </div><!-- panel-group -->
+		</div><!-- container -->
+	</section><!-- porque -->
 
 <?php include 'components/footer.php'; ?>
